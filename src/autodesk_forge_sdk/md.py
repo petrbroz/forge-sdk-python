a11776caa21f803f640f744c509669b9ebf23018
Petr Broz <ipetrbroz@gmail.com>
2021-05-05 15:17:44 +0200

    Added more Model Derivative APIs.

--- src/autodesk_forge_sdk/md.py
+++ src/autodesk_forge_sdk/md.py
@@ -3,11 +3,13 @@ from urllib.parse import quote
 from .auth import BaseOAuthClient, Scope, TokenProviderInterface, SimpleTokenProvider, OAuthTokenProvider
 
 BASE_URL = 'https://developer.api.autodesk.com/modelderivative/v2'
+READ_SCOPES = [Scope.DataRead, Scope.ViewablesRead]
 WRITE_SCOPES = [Scope.DataCreate, Scope.DataWrite, Scope.DataRead]
 
 def urnify(text):
     """
-    Convert input string into base64-encoded string (without padding '=' characters) that can be used as Model Derivative service URN.
+    Convert input string into base64-encoded string (without padding '=' characters) that can be used
+    as Model Derivative service URN.
 
     Args:
         text (str): Input text.
@@ -104,8 +106,9 @@ class ModelDerivativeClient(BaseOAuthClient):
             ```
             FORGE_CLIENT_ID = os.environ["FORGE_CLIENT_ID"]
             FORGE_CLIENT_SECRET = os.environ["FORGE_CLIENT_SECRET"]
+            URN = urnify("some-object-id")
             client = ModelDerivativeClient(OAuthTokenProvider(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET))
-            job = client.submit_job(urn, [{ "type": "svf", views: ["2d", "3d"] }], "US")
+            job = client.submit_job(URN, [{ "type": "svf", views: ["2d", "3d"] }], "US")
             print(job)
             ```
         """
@@ -132,3 +135,172 @@ class ModelDerivativeClient(BaseOAuthClient):
             headers['x-ads-force'] = 'true'
         # TODO: what about the EMEA endpoint?
         return self._post('/designdata/job', WRITE_SCOPES, json=json, headers=headers).json()
+
+    def get_thumbnail(self, urn: str, width: int=None, height: int=None) -> bytes:
+        """
+        Download thumbnail for a source file.
+
+        **Documentation**: https://forge.autodesk.com/en/docs/model-derivative/v2/reference/http/urn-thumbnail-GET
+
+        Args:
+            urn (str): Base64-encoded ID of the source file.
+            width (int, optional): Width of thumbnail. Possible values: 100, 200, 400.
+
+                If width is omitted, but height is specified, the implicit value for width will match height. If both
+                width and height are omitted, the server will return a thumbnail closest to a width of 200, if available.
+            height (int, optional): Height of thumbnail. Possible values: 100, 200, 400.
+
+                If height is omitted, but width is specified, the implicit value for height will match width. If both
+                width and height are omitted, the server will return a thumbnail closest to a width of 200, if available.
+
+        Returns:
+            bytes: buffer containing the thumbnail PNG image.
+
+        Examples:
+            ```
+            FORGE_CLIENT_ID = os.environ["FORGE_CLIENT_ID"]
+            FORGE_CLIENT_SECRET = os.environ["FORGE_CLIENT_SECRET"]
+            URN = urnify("some-object-id")
+            client = ModelDerivativeClient(OAuthTokenProvider(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET))
+            png = client.get_thumbnail(URN)
+            with open("thumbnail.png", "wb") as output:
+                output.write(png)
+            ```
+        """
+        params = {}
+        if width:
+            params['width'] = width
+        if height:
+            params['height'] = height
+        # TODO: what about the EMEA endpoint?
+        resp = self._get('/designdata/{}/thumbnail'.format(urn), READ_SCOPES, params=params)
+        return resp.content
+
+    def get_manifest(self, urn: str) -> dict:
+        """
+        Retrieve the manifest for the source design specified by the urn URI parameter.
+        The manifest is a list containing information about the derivatives generated while translating a source file.
+        The manifest contains information such as the URNs of the derivatives, the translation status of each derivative,
+        and much more.
+
+        **Documentation**: https://forge.autodesk.com/en/docs/model-derivative/v2/reference/http/urn-manifest-GET
+
+        Args:
+            urn (str): Base64-encoded ID of the source file.
+
+        Returns:
+            dict: Parsed manifest JSON.
+
+        Examples:
+            ```
+            FORGE_CLIENT_ID = os.environ["FORGE_CLIENT_ID"]
+            FORGE_CLIENT_SECRET = os.environ["FORGE_CLIENT_SECRET"]
+            URN = urnify("some-object-id")
+            client = ModelDerivativeClient(OAuthTokenProvider(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET))
+            manifest = client.get_manifest(URN)
+            print(manifest)
+            ```
+        """
+        # TODO: what about the EMEA endpoint?
+        return self._get('/designdata/{}/manifest'.format(urn), READ_SCOPES).json()
+
+    def delete_manifest(self, urn: str):
+        """
+        Delete the manifest and all its translated output files (derivatives). However, it does not delete
+        the design source file.
+
+        **Documentation**: https://forge.autodesk.com/en/docs/model-derivative/v2/reference/http/urn-manifest-DELETE
+
+        Args:
+            urn (str): Base64-encoded ID of the source file.
+
+        Examples:
+            ```
+            FORGE_CLIENT_ID = os.environ["FORGE_CLIENT_ID"]
+            FORGE_CLIENT_SECRET = os.environ["FORGE_CLIENT_SECRET"]
+            URN = urnify("some-object-id")
+            client = ModelDerivativeClient(OAuthTokenProvider(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET))
+            client.delete_manifest(URN)
+            ```
+        """
+        # TODO: what about the EMEA endpoint?
+        self._delete('/designdata/{}/manifest'.format(urn), WRITE_SCOPES)
+
+    def get_metadata(self, urn: str) -> dict:
+        """
+        Returns a list of model view (metadata) IDs for a design model. The metadata ID enables end users
+        to select an object tree and properties for a specific model view.
+
+        **Documentation**: https://forge.autodesk.com/en/docs/model-derivative/v2/reference/http/urn-metadata-GET
+
+        Args:
+            urn (str): Base64-encoded ID of the source file.
+
+        Returns:
+            dict: Parsed response JSON.
+
+        Examples:
+            ```
+            FORGE_CLIENT_ID = os.environ["FORGE_CLIENT_ID"]
+            FORGE_CLIENT_SECRET = os.environ["FORGE_CLIENT_SECRET"]
+            URN = urnify("some-object-id")
+            client = ModelDerivativeClient(OAuthTokenProvider(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET))
+            metadata = client.get_metadata(URN)
+            print(metadata)
+            ```
+        """
+        # TODO: what about the EMEA endpoint?
+        return self._get('/designdata/{}/metadata'.format(urn), READ_SCOPES).json()
+
+    def get_viewable_tree(self, urn: str, guid: str) -> dict:
+        """
+        Return an object tree, i.e., a hierarchical list of objects for a model view.
+
+        **Documentation**: https://forge.autodesk.com/en/docs/model-derivative/v2/reference/http/urn-metadata-guid-GET
+
+        Args:
+            urn (str): Base64-encoded ID of the source file.
+            guid (str): ID of one of the viewables extracted from the source file.
+
+        Returns:
+            dict: Parsed response JSON.
+
+        Examples:
+            ```
+            FORGE_CLIENT_ID = os.environ["FORGE_CLIENT_ID"]
+            FORGE_CLIENT_SECRET = os.environ["FORGE_CLIENT_SECRET"]
+            URN = urnify("some-object-id")
+            client = ModelDerivativeClient(OAuthTokenProvider(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET))
+            tree = client.get_viewable_tree(URN, "some-viewable-guid")
+            print(tree)
+            ```
+        """
+        # TODO: what about the EMEA endpoint?
+        return self._get('/designdata/{}/metadata/{}'.format(urn, guid), READ_SCOPES).json()
+
+    def get_viewable_properties(self, urn: str, guid: str) -> dict:
+        """
+        Return a list of properties for each object in an object tree. Properties are returned according to object ID
+        and do not follow a hierarchical structure.
+
+        **Documentation**: https://forge.autodesk.com/en/docs/model-derivative/v2/reference/http/urn-metadata-guid-properties-GET
+
+        Args:
+            urn (str): Base64-encoded ID of the source file.
+            guid (str): ID of one of the viewables extracted from the source file.
+
+        Returns:
+            dict: Parsed response JSON.
+
+        Examples:
+            ```
+            FORGE_CLIENT_ID = os.environ["FORGE_CLIENT_ID"]
+            FORGE_CLIENT_SECRET = os.environ["FORGE_CLIENT_SECRET"]
+            URN = urnify("some-object-id")
+            client = ModelDerivativeClient(OAuthTokenProvider(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET))
+            props = client.get_viewable_properties(URN, "some-viewable-guid")
+            print(props)
+            ```
+        """
+        # TODO: what about the EMEA endpoint?
+        return self._get('/designdata/{}/metadata/{}/properties'.format(urn, guid), READ_SCOPES).json()
