d5867feb50afe995b06cd79999737fdade6b0e9f
Petr Broz <ipetrbroz@gmail.com>
2021-05-05 10:47:25 +0200

    Updated version to 0.0.3.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="autodesk-forge-sdk",
-    version="0.0.2",
+    version="0.0.3",
     author="Petr Broz",
     author_email="petr.broz@autodesk.com",
     description="Unofficial Autodesk Forge SDK for Python.",
