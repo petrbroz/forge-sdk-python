b4c5c1b6018205c418b38a09345de93ec530c692
Petr Broz <ipetrbroz@gmail.com>
2021-05-10 17:26:42 +0200

    Added hubs and projects listing.

--- src/autodesk_forge_sdk/__init__.py
+++ src/autodesk_forge_sdk/__init__.py
@@ -4,5 +4,5 @@ Clients for communicating with different Autodesk Forge services.
 
 from .auth import AuthenticationClient, Scope, get_authorization_url
 from .auth import TokenProviderInterface, SimpleTokenProvider, OAuthTokenProvider
-from .oss import OSSClient
+from .dm import OSSClient, DataManagementClient
 from .md import ModelDerivativeClient, urnify
--- tests/context.py
+++ tests/context.py
@@ -4,7 +4,8 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 from autodesk_forge_sdk import AuthenticationClient, Scope, get_authorization_url
 from autodesk_forge_sdk import OAuthTokenProvider, SimpleTokenProvider
-from autodesk_forge_sdk import OSSClient, ModelDerivativeClient, urnify
+from autodesk_forge_sdk import OSSClient, DataManagementClient
+from autodesk_forge_sdk import ModelDerivativeClient, urnify
 
 FORGE_CLIENT_ID = os.environ["FORGE_CLIENT_ID"]
 FORGE_CLIENT_SECRET = os.environ["FORGE_CLIENT_SECRET"]
