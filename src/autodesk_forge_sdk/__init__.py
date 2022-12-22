33bd64d8cef778de7d468eba8ab9d06fd8b8d02d
Magnus-Matias Isbrand Høirup <MI@COWI.com>
2022-12-22 16:45:19 +0100

    reviews endpoints

--- src/autodesk_forge_sdk/__init__.py
+++ src/autodesk_forge_sdk/__init__.py
@@ -9,3 +9,4 @@ from .md import ModelDerivativeClient, urnify
 from .docm import DocumentManagementClient
 from .accm import AccountManagementClient, AccountManagementClient_BIM360
 from .webhooks import WebhooksClient
+from .reviews import ReviewsClient
