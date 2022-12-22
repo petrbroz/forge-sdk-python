4e0ca7432bb0151765eecb39b6d4ec11869b3808
Magnus-Matias Isbrand Høirup <MI@COWI.com>
2022-12-22 13:29:07 +0100

    added batch update folder permissions

--- src/autodesk_forge_sdk/__init__.py
+++ src/autodesk_forge_sdk/__init__.py
@@ -6,6 +6,6 @@ from .auth import AuthenticationClient, Scope, get_authorization_url
 from .auth import TokenProviderInterface, SimpleTokenProvider, OAuthTokenProvider
 from .datam import OSSClient, DataManagementClient, DataRetention
 from .md import ModelDerivativeClient, urnify
-from .docm import DocumentManagementClient
+from .docm import DocumentManagementClient, Action, Subject
 from .accm import AccountManagementClient, AccountManagementClient_BIM360
 from .webhooks import WebhooksClient
--- tests/context.py
+++ tests/context.py
@@ -7,8 +7,9 @@ from autodesk_forge_sdk import OAuthTokenProvider, SimpleTokenProvider
 from autodesk_forge_sdk import OSSClient, DataManagementClient
 from autodesk_forge_sdk import ModelDerivativeClient, urnify
 from autodesk_forge_sdk import WebhooksClient
+from autodesk_forge_sdk import DocumentManagementClient, Action, Subject
 from autodesk_forge_sdk import AccountManagementClient, AccountManagementClient_BIM360
-from autodesk_forge_sdk import DocumentManagementClient
+
 FORGE_CLIENT_ID = os.environ["FORGE_CLIENT_ID"]
 FORGE_CLIENT_SECRET = os.environ["FORGE_CLIENT_SECRET"]
 FORGE_BUCKET = os.environ["FORGE_BUCKET"]
